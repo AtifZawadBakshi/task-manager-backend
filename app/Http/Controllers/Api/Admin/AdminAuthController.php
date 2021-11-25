531bd42445591f5f92b1f3b3cb16388e0e6b763c
sofen <md.rabby.mahmud@gmail.com>
2021-11-25 11:26:01 +0600

    Admin controller ok

--- app/Http/Controllers/Api/Admin/AdminAuthController.php
+++ app/Http/Controllers/Api/Admin/AdminAuthController.php
@@ -3,16 +3,9 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Models\Admin;
-// use Dotenv\Validator;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-
-// use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
-// use App\Models\Admin;
-// use Illuminate\Support\Facades\Hash;
-// use Illuminate\Support\Facades\Validator;
 use JWTAuth;
 use Session;
 use Tymon\JWTAuth\Exceptions\JWTException;
@@ -95,56 +88,4 @@ class AdminAuthController extends Controller
         
         return response()->json(['status' => true, 'message' => 'Successfully logged out']);
     }
-
-    // public function register(Request $request) {
-    //     $fields = $request->validate([
-    //         // 'name' => 'required|string',
-    //         // 'email' => 'required|string|unique:users,email',
-    //         // 'password' => 'required|string|confirmed'
-    //     ]);
-
-    //     $user = Admin::create([
-    //         'name' => $fields['name'],
-    //         'email' => $fields['email'],
-    //         'password' => bcrypt($fields['password']),
-    //     ]);
-
-    //     $token = $user->createToken('myapptoken')->plainTextToken;
-
-    //     $response = [
-    //         'user' => $user,
-    //         'token' => $token
-    //     ];
-    //     // return response($response, 201);
-    //     return $result = response()->json($response, 201);
-    //     // return response()-json(['ok',201]);
-    // }
-
-
-    // public function login(Request $request){
-
-    //     $fields = $request->validate([
-    //         'email' => 'required|string',
-    //         'password' => 'required|string'
-    //     ]);
-
-    //     // Check email
-    //     $user = Admin::where('email', $fields['email'])->first();
-
-    //     // Check password
-    //     if(!$user || !Hash::check($fields['password'], $user->password)) {
-    //         return response([
-    //             'message' => 'Bad creds'
-    //         ], 401);
-    //     }
-
-    //     $token = $user->createToken('myapptoken')->plainTextToken;
-
-    //     $response = [
-    //         'user' => $user,
-    //         'token' => $token
-    //     ];
-
-    //     return response()->json($response, 201);
-    // }
 }
